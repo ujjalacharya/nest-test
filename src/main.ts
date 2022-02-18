6a39bc9389ce0f2f0cc449eae8b0c8af339976c7
Ujjal Acharya <acharyaujjal1@gmail.com>
2022-02-18 20:29:18 +0545

    add code for swagger implementation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+  .setTitle('NEST POST API')
+  .setVersion('1.0')
+  .addBearerAuth()
+  .build();
+
+const document = SwaggerModule.createDocument(app, config);
+SwaggerModule.setup('docs', app, document, {
+  swaggerOptions: {
+    persistAuthorization: true,
+  },
+});
   await app.listen(3000);
 }
 bootstrap();
