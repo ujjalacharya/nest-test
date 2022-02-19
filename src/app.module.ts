3c361d861fdae132761e8a214c3764c34b3a248c
Ujjal Acharya <acharyaujjal1@gmail.com>
2022-02-19 11:56:06 +0545

    add post files

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { PostsModule } from './posts/posts.module';
 
 @Module({
   imports: [
@@ -12,7 +13,8 @@ import { UsersModule } from './users/users.module';
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
     }),
-    UsersModule
+    UsersModule,
+    PostsModule
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ src/posts/entities/post.entity.ts
@@ -0,0 +1,15 @@
+import { User } from "src/users/entities/user.entity";
+import Model from "src/utils/basemodel.entity";
+import { Column, Entity, ManyToOne } from "typeorm";
+
+@Entity('posts')
+export class PostModel extends Model {
+
+@Column()
+content: string
+
+// Relationships
+@ManyToOne(() => User, (user) => user.post)
+user: User
+
+}
